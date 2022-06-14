0cf7b7fc03a0b96e523758865bcc50b9d32a7f53
Rafael Oliveira Cruz <rafaeloliveira@altamogiana.com.br>
2022-06-14 10:19:17 -0300

    Commit feita pelo Paulo no apps js

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando com o banco de dados")
+// mais um console log de teste:
+console.log("O Paulo fez esse commit sem você ver na aula!")
